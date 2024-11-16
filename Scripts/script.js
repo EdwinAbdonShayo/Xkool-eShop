169f27f219709dec679fa0402f3982c15d32b2dd
EdwinAbdonShayo <contact@edwinshayo.com>
2024-11-16 16:08:22 +0400

    Updated script.js, styles.css, and index.html files with changes to cart functionality, button styles, and checkout section layout.

--- Scripts/script.js
+++ Scripts/script.js
@@ -17,12 +17,10 @@ let xkoolWebstore = new Vue({
     },
     methods: {
         addItemtoCart(program) {
-            this.cart.push(program);
+            this.cart.push(program.id);
         },
         removeFromCart(program) {
-            this.cart.splice(
-                this.cart.indexOf(program), 1
-            );
+            this.cart.splice(this.cart.indexOf(program.id), 1);
         },
         showCheckOut() {
             this.showProduct = !this.showProduct;
@@ -31,21 +29,31 @@ let xkoolWebstore = new Vue({
             alert("Order placed successfully!");
         },
         canAddtoCart(program) {
-            return program.availableInventory > this.cartCount(program);
+            return program.availableInventory > this.cartCount(program.id);
         },
         canRemovefromCart(program) {
-            return this.cartCount(program) > 0;
+            return this.cartCount(program.id) > 0;
         },
         cartCount(id) {
             return this.cart.filter(itemId => itemId === id).length;
         },
         itemsLeft(program) {
-            return program.availableInventory - this.cartCount(program);
+            return program.availableInventory - this.cartCount(program.id);
         }
     },
     computed: {
         sortedPrograms() {
             return this.products.sort((a, b) => a.price - b.price);
+        },
+        // Returns an array of detailed program objects currently in the cart
+        cartDetails() {
+            return this.cart.map(cartItemId => {
+                return this.products.find(program => program.id === cartItemId);
+            });
+        },
+        // Calculates the total price of all items in the cart
+        totalPrice() {
+            return this.cartDetails.reduce((total, program) => total + program.price, 0);
         }
     }
 });
